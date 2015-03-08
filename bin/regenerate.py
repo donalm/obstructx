517a92683efa497e37a68e2b94de6c94e4166ef5
Dónal McMullan <donal.mcmullan@gmail.com>
2015-03-08 19:39:24 -0400

    Starting a regenerate script

---
+++ bin/regenerate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+basedir = "/home/donal/Geek/obstructx"
+
+import os
+import sys
+sys.path.insert(0, os.path.join(basedir, "lib/python"))
+
+from obstructx import inspect
+# print inspect.queries.keys()
+# ['list_foreign_keys', 'list_table_indexes', 'list_databases', 'list_table_fields', 'list_tables']
+
+
+
+
+
+
+
+
+
