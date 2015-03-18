1c5c0279ae4806b5f29794a6693c0310d215a8bf
Dónal McMullan <donal.mcmullan@gmail.com>
2015-03-17 21:12:41 -0400

    Once the log has been setup once, other modules can call it without the appname argument

--- lib/python/obstructx/log.py
+++ lib/python/obstructx/log.py
@@ -6,22 +6,32 @@ from logging.handlers import TimedRotatingFileHandler
 
 import logging
 
-def get_logger(appname):
-    config = Config()
-    log_config         = config.get(appname, "log")
-    log_level          = log_config.get("level")
-    log_path_templates = log_config.get("paths")
-    log_path           = get_log_path(appname, log_path_templates)
+def get_logger(appname=None):
+    return AppLogger.get(appname)
 
-    formatter = logging.Formatter('%(asctime)s: %(levelname)s : %(message)s    [%(process)d:%(pathname)s:%(lineno)d]')
+class AppLogger(object):
+    logger = None
 
-    trfh = TimedRotatingFileHandler(log_path, when='D', interval=1, backupCount=5, encoding='utf-8')
-    trfh.setFormatter(formatter)
-    logger = logging.getLogger(appname)
-    logger.addHandler(trfh)
-    logger.error("trfh")
-    logger.setLevel(logging.DEBUG)
-    return logger
+    @classmethod
+    def get(cls, appname):
+        if appname is None:
+            return cls.logger
+
+        config = Config()
+        log_config         = config.get(appname, "log")
+        log_level          = log_config.get("level")
+        log_path_templates = log_config.get("paths")
+        log_path           = get_log_path(appname, log_path_templates)
+
+        formatter = logging.Formatter('%(asctime)s: %(levelname)s : %(message)s    [%(process)d:%(pathname)s:%(lineno)d]')
+
+        trfh = TimedRotatingFileHandler(log_path, when='D', interval=1, backupCount=5, encoding='utf-8')
+        trfh.setFormatter(formatter)
+        cls.logger = logging.getLogger(appname)
+        cls.logger.addHandler(trfh)
+        cls.logger.error("trfh")
+        cls.logger.setLevel(logging.DEBUG)
+        return cls.logger
 
 def get_log_path(appname, log_path_templates):
     for log_path_template in log_path_templates:
